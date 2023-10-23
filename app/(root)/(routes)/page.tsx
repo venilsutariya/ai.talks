47fa7f7989543adbc9ef98d991cec9fbc70f2bb6
VenilSutariya_ <venilsutariya7@gmail.com>
2023-10-23 15:54:14 +0530

    add code

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const RootPage = () => {
+    return ( 
+        <div>
+            <UserButton />
+        </div>
+     );
+}
+ 
+export default RootPage;
